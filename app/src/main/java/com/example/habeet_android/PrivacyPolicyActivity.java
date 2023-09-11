d6db651673292c349d21d3cd6127df4b38558d53
TECNB <3489044730@qq.com>
2023-09-11 13:35:12 +0800

    添加了tag和helper

--- app/src/main/java/com/example/habeet_android/PrivacyPolicyActivity.java
+++ app/src/main/java/com/example/habeet_android/PrivacyPolicyActivity.java
@@ -9,6 +9,6 @@ public class PrivacyPolicyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_privacy_policy_view);
+        setContentView(R.layout.activity_privacy_policy);
     }
 }
